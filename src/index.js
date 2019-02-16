8d049824a28cd1494c9b2d13852b5c43a8e22457
Lucas Valandro <lvalandrorocha@hotmail.com>
2019-02-16 13:22:43 -0200

    [DAILY-TIME]
    Added cron job implementation to always remind your team that is the dailys time

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const SlackBot = require('slackbots');
-const axios = require('axios');
+const CronJob = require('cron').CronJob; 
 
 const bot = new SlackBot({
     token: process.env.SLACK_TOKEN,
@@ -11,6 +11,11 @@ bot.on('start', () => {
         icon_emoji: ':robot_face:'
     };
 
-    bot.postMessageToChannel('general', 'Olá, meu nome é @daily-bot! \n' + 
-                                        'Estou aqui para lembra-los de quando a hora da daily estaá chegando.', params);
+    bot.postMessageToChannel('general', 'Olá, meu nome é @daily-bot! \n\n' + 
+                                        'Estou aqui para lembra-los de quando a hora da daily está chegando.', params);
 });
+
+// Cron job to set your Daily's time
+new CronJob('00 30 9 * * 1-5', () => {
+    bot.postMessageToChannel('general', 'Está na hora da daily!');
+},null,true,'America/Sao_Paulo');
