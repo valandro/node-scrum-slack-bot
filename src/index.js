54045b49e8d431a95b2f073e9ae521a5ac8e1ef7
Lucas Valandro <lvalandrorocha@hotmail.com>
2019-02-22 21:00:50 -0300

    [TEAM-B]
    Add team B messages to remind them about daily's time.

--- src/index.js
+++ src/index.js
@@ -6,17 +6,26 @@ const bot = new SlackBot({
     name: 'daily-bot'
 });
 
-// On start event
+// Welcome message
 bot.on('start', () => {
     const params = {
         icon_emoji: ':robot_face:'
     };
 
-    bot.postMessageToChannel('general', 'Olá, meu nome é @daily-bot! \n\n' + 
-                                        'Estou aqui para lembra-los de quando a hora da daily está chegando.', params);
+    bot.postMessageToChannel(
+        'omni_pdv-time_b', 
+        'Olá, meu nome é *@daily-bot!* \n\n'
+        + 'Estou aqui para lembra-los de quando a hora da daily está chegando!!! \n\n'
+        + 'Por enquanto, apenas consigo alertar sobre os horários da daily, mas qualquer sugestão de melhoria é bem vinda.'
+        , params);
 });
 
-// Cron job to set your Daily's time
-new CronJob('00 30 11 * * 1-5', () => {
-    bot.postMessageToChannel('general', 'Está na hora da daily!');
+// Team B daily's is coming. 
+new CronJob('00 55 9 * * 1-5', () => {
+    bot.postMessageToChannel('omni_pdv-time_b', 'Está quase na hora da daily, faltam apenas *5 minutos!* :eyes:');
+},null,true,'America/Sao_Paulo');
+
+// Team B daily's is now. 
+new CronJob('00 00 10 * * 1-5', () => {
+    bot.postMessageToChannel('omni_pdv-time_b', '*Está na hora da daily!* :the_horns:');
 },null,true,'America/Sao_Paulo');
