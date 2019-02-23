44d4aaa128682cf92b1aae78d5e4a1ab38b48947
Lucas Valandro <lvalandrorocha@hotmail.com>
2019-02-22 21:10:57 -0300

    [TEAM-A]
    Add team A reminders to daily's time

--- src/index.js
+++ src/index.js
@@ -13,19 +13,36 @@ bot.on('start', () => {
     };
 
     bot.postMessageToChannel(
+        'omni_pdv_time_a', 
+        'Olá, meu nome é *@daily-bot!* \n\n'
+        + 'Estou aqui para lembrá-los de quando a hora da daily está chegando!!! \n\n'
+        + 'Por enquanto, apenas consigo alertar sobre os horários da daily, mas qualquer sugestão de melhoria é bem vinda.'
+        , params);
+
+    bot.postMessageToChannel(
         'omni_pdv-time_b', 
         'Olá, meu nome é *@daily-bot!* \n\n'
-        + 'Estou aqui para lembra-los de quando a hora da daily está chegando!!! \n\n'
+        + 'Estou aqui para lembrá-los de quando a hora da daily está chegando!!! \n\n'
         + 'Por enquanto, apenas consigo alertar sobre os horários da daily, mas qualquer sugestão de melhoria é bem vinda.'
         , params);
 });
 
-// Team B daily's is coming. 
+// Team A daily's is coming.
+new CronJob('00 25 9 * * 1-5', () => {
+    bot.postMessageToChannel('omni_pdv_time_a', 'Está quase na hora da daily, faltam apenas *5 minutos!* :eyes:');
+},null,true,'America/Sao_Paulo');
+
+// Team A daily's is now.
+new CronJob('00 30 9 * * 1-5', () => {
+    bot.postMessageToChannel('omni_pdv_time_a', '*Está na hora da daily!* :the_horns:');
+},null,true,'America/Sao_Paulo');
+
+// Team B daily's is coming.
 new CronJob('00 55 9 * * 1-5', () => {
     bot.postMessageToChannel('omni_pdv-time_b', 'Está quase na hora da daily, faltam apenas *5 minutos!* :eyes:');
 },null,true,'America/Sao_Paulo');
 
-// Team B daily's is now. 
+// Team B daily's is now.
 new CronJob('00 00 10 * * 1-5', () => {
     bot.postMessageToChannel('omni_pdv-time_b', '*Está na hora da daily!* :the_horns:');
 },null,true,'America/Sao_Paulo');
